d11a7dfd67f203042e6e184452127cc59da8b64a
temesgen-abebayehu <temesgensoftware21@gmail.com>
2025-08-05 16:44:21 +0300

    refine email activation functionality

--- delivery/controller/user_controller.go
+++ delivery/controller/user_controller.go
@@ -30,8 +30,9 @@ func (uc *UserController) ChangeUserRole(c *gin.Context, roleChange func(context
 		return
 	}
 	ctx := c.Request.Context()
-	if err := roleChange(ctx, req.Email); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	err := uc.userUsecase.Promote(ctx, req.Email)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, err)
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": successMessage})
@@ -41,7 +42,19 @@ func (uc *UserController) HandlePromote(c *gin.Context) {
 }
 
 func (uc *UserController) HandleDemote(c *gin.Context) {
-	uc.ChangeUserRole(c, uc.userOperations.Demote, "user demoted successfully")
+	var req emailRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.IndentedJSON(http.StatusNotFound, err)
+		return
+	}
+
+	ctx := c.Request.Context()
+	err := uc.userUsecase.Demote(ctx, req.Email)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, err)
+		return
+	}
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "user demoted successfully"})
 }
 
 func (uc *UserController) HandleUpdateUser(c *gin.Context) {
